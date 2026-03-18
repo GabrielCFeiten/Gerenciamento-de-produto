c7b37bac0186ba1312f985b84892f83a16e018ce
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-18 19:28:44 -0300

    ProdutoModel create

---
+++ src/main/java/org/cf/gerenciadordeproduto/GerenciadorDeProdutoApplication.java
@@ -0,0 +1,13 @@
+package org.cf.gerenciadordeproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeProdutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorDeProdutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cf/gerenciadordeproduto/GerenciadorDeProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cf.gerenciadordeproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeProdutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
